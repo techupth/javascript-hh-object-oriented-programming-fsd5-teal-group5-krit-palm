d300c0cb690e241e8df63a6c4e1c0e095ef7a620
Krit <titatit.akkh@gmail.com>
2023-12-15 15:07:04 +0700

    feat: finish Ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,31 @@
-class EmailNotification { }
+class EmailNotification { 
+    
+    constructor(notificationId, createdTime, content, receiver){
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
 
-class SMSNotification { }
+    send() {
+        return "Notification has been sent to <email>"
+    }
+}
+
+class SMSNotification { 
+    constructor(notificationId, createdTime, content, phoneNumber){
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.phoneNumber = phoneNumber;
+    }
+
+    send(){
+        return `Notification has been sent to ${this.phoneNumber}`
+    }
+}
+
+const bill = new EmailNotification("00", "18:00", "Guest", "mail")
+console.log(bill.send())
+const nael = new SMSNotification("01", "8:00", "Host", "xxx-xxx-xxxx")
+console.log(nael.send())
